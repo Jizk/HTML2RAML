5c3b9d1eef4276090c32cb12644ac05f5582282a
Jizk <Jizk666@163.com>
2019-11-28 18:29:40 +0800

    fix

--- HtmlToRaml.php
+++ HtmlToRaml.php
@@ -22,9 +22,9 @@ class HtmlToRaml
         $dom = new Dom;
         $dom->loadStr($html, []);
 
-        if (empty($dom->root->firstChild()->getChildren()[1])){
+        if (empty($dom->root->firstChild()->getChildren()[1])) {
             $root = $dom->root->firstChild()->getChildren()[0];
-        }else{
+        } else {
             $root = $dom->root->firstChild()->getChildren()[1];
         }
 
@@ -47,62 +47,62 @@ class HtmlToRaml
         /**
          * @var Dom\HtmlNode $item
          */
-        foreach ($children as $item){
+        foreach ($children as $item) {
             $outerHtml = Utility::Instance()->trimContent($item->outerHtml());
-            if (empty($outerHtml)){
+            if (empty($outerHtml)) {
                 continue;
             }
             $tag = strtolower($item->getTag()->name());
-            if (!empty($outerHtml)){
+            if (!empty($outerHtml)) {
                 if ($tag == 'p'
                     || $tag == 'img'
                     || $tag == 'ul'
                     || in_array($tag, self::$hTagNameArray)
                     || $tag == 'video'
-                ){
+                ) {
                     $innerContent = $item->innerHtml();
                     $sourceContent = Utility::Instance()->trimContent(strip_tags($innerContent));
-                    if (!empty($sourceContent)){
-                        if ($tag == 'p'){
+                    if (!empty($sourceContent)) {
+                        if ($tag == 'p') {
                             $align = Utility::Instance()->getCssValueFromItem($item, 'text\-align');
                             $id = Utility::Instance()->getId($item);
                             $pAml = Builder::Instance()->buildTextNode($sourceContent, '', $align, $id);
                             Utility::Instance()->parseMarkUp($item, $pAml);
 //                            Utility::Instance()->parseSentence($item, $pAml);
                             $ret[] = $pAml;
-                        }elseif ($tag == 'ul') {
+                        } elseif ($tag == 'ul') {
                             $ulChildren = Utility::Instance()->getChildrenByTag($item, 'li');
                             $order = 0;
                             /**
                              * @var Dom\HtmlNode $child
                              */
-                            foreach ($ulChildren as $child){
+                            foreach ($ulChildren as $child) {
                                 $id = Utility::Instance()->getId($child);
                                 $childrenItem = Utility::Instance()->trimContent(strip_tags($child->innerHtml()));
                                 $pAml = Builder::Instance()->buildLiNode($childrenItem, $id, ++$order);
                                 Utility::Instance()->parseMarkUp($child, $pAml);
                                 $ret[] = $pAml;
                             }
-                        }elseif (in_array($tag, self::$hTagNameArray)){
+                        } elseif (in_array($tag, self::$hTagNameArray)) {
                             $align = Utility::Instance()->getCssValueFromItem($item, 'text\-align');
                             $id = Utility::Instance()->getId($item);
                             $pAml = Builder::Instance()->buildTextNode($sourceContent, $tag, $align, $id);
                             Utility::Instance()->parseMarkUp($item, $pAml);
                             $ret[] = $pAml;
                         }
-                    }elseif ($tag == 'img'){
+                    } elseif ($tag == 'img') {
                         $id = Utility::Instance()->getId($item);
                         $pAml = Builder::Instance()->buildImgNode($item, $id);
                         $ret[] = $pAml;
-                    }elseif ($tag == 'video'){
+                    } elseif ($tag == 'video') {
                         $id = Utility::Instance()->getId($item);
                         $pAml = Builder::Instance()->buildVideoNode($item, $id);
                         $ret[] = $pAml;
-                    }elseif (strpos($innerContent, "</a>") !== false){ // <p><a><img /></a></p> 不能在下面解析嵌套的img标签的判断之后，会出错
+                    } elseif (strpos($innerContent, "</a>") !== false) { // <p><a><img /></a></p> 不能在下面解析嵌套的img标签的判断之后，会出错
                         $aChildren = Utility::Instance()->getChildrenByTag($item, 'a');
-                        foreach ($aChildren as $aChild){
+                        foreach ($aChildren as $aChild) {
                             $imgChildren = Utility::Instance()->getChildrenByTag($aChild, 'img');
-                            if ($imgChildren){
+                            if ($imgChildren) {
                                 foreach ($imgChildren as $imgChild) {
                                     $id = Utility::Instance()->getId($aChild);
                                     $pAml = Builder::Instance()->buildImgNode($imgChild, $id);
@@ -111,9 +111,9 @@ class HtmlToRaml
                                 }
                             }
                         }
-                    }elseif (strpos($innerContent, '<img') !== false){ // <p><img /></p>
+                    } elseif (strpos($innerContent, '<img') !== false) { // <p><img /></p>
                         $imgChildren = Utility::Instance()->getChildrenByTag($item, 'img');
-                        if ($imgChildren){
+                        if ($imgChildren) {
                             foreach ($imgChildren as $imgChild) {
                                 $id = Utility::Instance()->getId($item);
                                 $pAml = Builder::Instance()->buildImgNode($imgChild, $id);
@@ -128,7 +128,6 @@ class HtmlToRaml
         return $ret;
 //        return json_encode($ret, 256);
     }
-
 
 
 }
